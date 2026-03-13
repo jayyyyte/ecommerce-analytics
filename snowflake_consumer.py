1e2d10aec17c599518d354c386b6a7af86a79b62
jayyyyte <138153456+jayyyyte@users.noreply.github.com>
2026-03-14 03:06:29 +0700

    initial commit

---
+++ snowflake_consumer.py
@@ -0,0 +1,71 @@
+import json
+from kafka import KafkaConsumer
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
+import pandas as pd
+
+BOOTSTRAP_SERVERS="host.docker.internal:29092"
+TOPIC_NAME="clean_events"
+GROUP_ID = "snowflake-loader"
+
+SNOWFLAKE_CONFIG = {
+    "user" : "<<USERNAME>>",
+    "password" : "<<PASSWORD>>",
+    "account": "<<ACCOUNT>>",
+    "warehouse" : "COMPUTE_WH",
+    "database" : "KAFKA_DB",
+    "schema" : "STREAMING"
+}
+
+BATCH_SIZE = 10
+
+consumer = KafkaConsumer(
+    TOPIC_NAME,
+    bootstrap_servers=BOOTSTRAP_SERVERS,
+    group_id=GROUP_ID,
+    enable_auto_commit=False,
+    auto_offset_reset="earliest",
+    key_deserializer=lambda k: k.decode("utf-8") if k else None,
+    value_deserializer=lambda v: json.loads(v.decode("utf-8"))
+)
+
+sf_conn = snowflake.connector.connect(**SNOWFLAKE_CONFIG)
+
+print("Connected to Snowflake")
+print("Starting Kafka -> Snowflake Loader...")
+
+buffer = []
+
+def flush_to_snowflake(records):
+    df = pd.DataFrame(records)
+    df.columns = [c.upper() for c in df.columns]
+
+    success, nchunks, nrows, _ = write_pandas(
+        conn=sf_conn,
+        df=df,
+        table_name="KAFKA_EVENTS_SILVER"
+    )
+
+    if not success:
+        raise Exception("Snowflake insert failed")
+    
+    print(f"Inserted {nrows} rows into Snowflake")
+
+for message in consumer:
+    event = message.value
+    buffer.append({
+        "event_id": event["event_id"],
+        "customer_id" : event["customer_id"],
+        "event_type" : event["event_type"],
+        "amount" : event["amount"],
+        "currency" : event["currency"],
+        "event_timestamp" : event["event_timestamp"]
+    })
+
+    if len(buffer) >= BATCH_SIZE:
+        try:
+            flush_to_snowflake(buffer)
+            consumer.commit()
+            buffer.clear()
+        except EXCEPTION as e:
+            print(f"ERROR inserting batch: {e}")
